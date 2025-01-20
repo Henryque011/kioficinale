2fb932d0f3cf1033fa919dfa9a3d8fdf09943274
henryque <henryquenonatosilva@gmail.com>
2025-01-20 11:33:28 -0300

    tst

--- app/views/dash/servico/adicionar.php
+++ app/views/dash/servico/adicionar.php
@@ -4,9 +4,8 @@
 <a href="http://localhost/KiOficinale/public/servicos/editar">EDITAR</a>
 <a href="http://localhost/KiOficinale/public/servicos/desativar">DESATIVAR</a>
 <a href="http://localhost/KiOficinale/public/servicos/listar">LISTAR</a>
-<a href="http://localhost/KiOficinale/public/servicos/listar">LISTAR</a>
 
-<h2>edede</h2>
+<h2>teste 2</h2>
 
-<img src="public/assets/img/sem-foto-cliente1" alt="">
+<img src="public/assets/img/sem-foto-cliente1" alt="#">
 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas asperiores necessitatibus in tenetur perferendis rerum odit minus deleniti excepturi quis doloremque ipsam minima, illum quas, nobis harum nostrum fuga. Quo!</p>
