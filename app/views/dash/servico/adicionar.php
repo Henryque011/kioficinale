2873a13cf0729a1ebf8a47f2826901b8ab041de4
henryque <henryquenonatosilva@gmail.com>
2025-01-20 11:16:25 -0300

    add

--- app/views/dash/servico/adicionar.php
+++ app/views/dash/servico/adicionar.php
@@ -1,6 +1,10 @@
-<H1>PG ADICIONAR</H1>
-<!-- test -->
+<H1>PG ADICIONAR </H1>
+
 <!-- <a href="http://localhost/KiOficinale/public/servicos/adicionar">ADICIONAR</a> -->
 <a href="http://localhost/KiOficinale/public/servicos/editar">EDITAR</a>
 <a href="http://localhost/KiOficinale/public/servicos/desativar">DESATIVAR</a>
 <a href="http://localhost/KiOficinale/public/servicos/listar">LISTAR</a>
+<h2>edede</h2>
+
+<img src="public/assets/img/sem-foto-cliente1" alt="">
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas asperiores necessitatibus in tenetur perferendis rerum odit minus deleniti excepturi quis doloremque ipsam minima, illum quas, nobis harum nostrum fuga. Quo!</p>
