98ea1cfa42c40b236bb87a40ce5de0bb19c705ea
henryque <henryquenonatosilva@gmail.com>
2025-01-21 08:13:44 -0300

    a

--- app/views/dash/servico/adicionar.php
+++ app/views/dash/servico/adicionar.php
@@ -18,7 +18,7 @@
             <label for="exampleInputPassword1" class="form-label">Descrição do Serviço:</label>
             <input type="password" class="form-control" id="exampleInputPassword1">
         </div>
-        <div class="side "style="display: flex; margin: 20px;">
+        <div class="side " style="display: flex; margin: 20px;">
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Preço Base:</label>
                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
@@ -45,6 +45,52 @@
             <input type="checkbox" class="form-check-input" id="exampleCheck1">
             <label class="form-check-label" for="exampleCheck1">t</label> 
         </div> -->
+        <form>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputEmail4">Email</label>
+                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputPassword4">Password</label>
+                    <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="inputAddress">Address</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+            </div>
+            <div class="form-group">
+                <label for="inputAddress2">Address 2</label>
+                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputCity">City</label>
+                    <input type="text" class="form-control" id="inputCity">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="inputState">State</label>
+                    <select id="inputState" class="form-control">
+                        <option selected>Choose...</option>
+                        <option>...</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="inputZip">Zip</label>
+                    <input type="text" class="form-control" id="inputZip">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="gridCheck">
+                    <label class="form-check-label" for="gridCheck">
+                        Check me out
+                    </label>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Sign in</button>
+        </form>
         <button type="submit" class="btn btn-primary">Salvar</button>
         <button type="submit" class="btn btn-second " style="background-color: grey;  color: white;">Cancelar</button>
     </form>
